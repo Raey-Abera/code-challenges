3ac08f64bdfdd64b2101f4e7fb9e04fd84642b6c
Raey-Abera <47502899+Raey-Abera@users.noreply.github.com>
2020-10-30 22:39:53 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-//write a function that takes in an array, sorts it, and removes duplcates from the array 
+let items = [5, 33, 7, 6, 2, 11]
 
-const hasDuplicates = arr => {
-    return new Set(arr).size !== arr.length;
+const swap = (items, leftIndex, rightIndex) => {
+    let temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
 }
 
-const merge = (left, right) => { 
-    const results = [];
-    while(left.length && right.length) {
-        if(left[0] < right[0]){
-            results.push(left.shift())
-        }else {
-            results.push(right.shift())
+const partition = (items, left, right) => {
+    let pivot = items[Math.floor((right + left) / 2)],
+        i = left,
+        j = right;  
+
+    while (i <= j) {     
+        while (items[i] < pivot) {
+            i++; 
+        }
+        while (items[j] > pivot) {
+            j--; 
+        }
+        if (i <= j) {
+            swap(items, i, j); 
+            i++;
+            j--;
         }
     }
-    return [...results, ...left, ...right]
+    return i;
 }
 
-const mergeSort = arr => {
-    if(arr.length <= 1) return arr;
-    const center = Math.floor(arr.length / 2)
-    const left = arr.slice(0, center)
-    const right = arr.slice(center)
-    return merge(mergeSort(left), mergeSort(right))
-}
-
-const removeDuplicates = arr => {
-    if(!Array.isArray(arr)) return `This is not an array. This is a/an ${typeof arr}`
-    if(arr.length === 0) return 'This an empty array'
-    if(!hasDuplicates(arr)) return arr;
-    const sortedArr = mergeSort(arr.slice())
-    const results = []
-    for(let i = 0; i < sortedArr.length; i++) {
-        if(sortedArr[i + 1] !== sortedArr[i]){
-            results.push(sortedArr[i])
+const quickSort = (items, left, right) => {
+    let index;
+    if (items.length > 1) {
+        index = partition(items, left, right);
+        if (left < index - 1) {
+            quickSort(items, left, index - 1);
         }
+        if (index < right) {
+            quickSort(items, index, right);
+        }
+        return items;
     }
-    return results
 }
 
-console.log('test1', removeDuplicates([-3, 99, 1001, 1001, -3, 56, 27, 27, 27])); // -3, 27, 56, 99, 1001
-console.log('test2', removeDuplicates([])); //This an empty array
-console.log('test3', removeDuplicates({})) //This is not an array. This is a/an object
-console.log('test4', removeDuplicates('')) //This is not an array. This is a/an string
-console.log('test5', removeDuplicates(0)) //This is not an array. This is a/an number
+let sortedArray = quickSort(items, 0, items.length - 1)
+
+console.log(sortedArray); //prints[2, 5, 6, 7, 11, 33]
