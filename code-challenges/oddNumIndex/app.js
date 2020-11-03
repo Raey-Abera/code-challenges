7ecd57680887d44920093a173259e6948278cd75
Raey-Abera <ab.raey@gmail.com>
2020-11-03 09:51:22 -0500

    first commit

--- code-challenges/oddNumIndex/app.js
+++ code-challenges/oddNumIndex/app.js
@@ -1,12 +1,16 @@
 // Create a function that takes an array of numbers, and returns the index of the first odd number
 // If there are no odd numbers, the function should output -1. Numbers are guaranteed to be non-negative
 
-const oddNumIndex = numArr => {
-    for (i = 0; i < numArr.length; i++) {
-        if (numArr[i] % 2 !== 0)  return numArr.indexOf(numArr[i]);
-    }
-    return -1
-}
+// const oddNumIndex = numArr => {
+//     for (i = 0; i < numArr.length; i++) {
+//         if (numArr[i] % 2 !== 0)  return numArr.indexOf(numArr[i]);
+//     }
+//     return -1
+// }
+
+const oddNumIndex = numbers => {
+    return numbers.findIndex(num => num % 2 != 0);
+  }
 
 console.log('test1', oddNumIndex([2, 4, 6, 8, 10])) //-1
 console.log('test1', oddNumIndex([2, 4, 6, 7, 10])) //3
