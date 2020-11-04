c53f1ad54bc2baaf8130e5980f19276556d62882
Raey-Abera <ab.raey@gmail.com>
2020-11-04 15:14:48 -0500

    first commit

--- code-challenges/findShortestWord/app.js
+++ code-challenges/findShortestWord/app.js
@@ -1,5 +1,5 @@
-// Simple, given a string of words, return the length of the shortest word(s).
-// String will never be empty and you do not need to account for different data types.
+// Simple, given a string of words, return the length of the shortest word(s)
+// String will never be empty and you do not need to account for different data types
 
 const findShort = str => Math.min(...str.split(' ').map(str => str.length));
 
