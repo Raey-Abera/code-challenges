42999c6fae874ee1755e5cddfc0dfcd45b400b3c
Raey-Abera <ab.raey@gmail.com>
2020-11-05 19:15:55 -0500

    first commit

--- code-challenges/folder/app.js
+++ code-challenges/folder/app.js
@@ -1,7 +1,20 @@
 // Simple, given a string of words, return the length of the shortest word(s).
 // String will never be empty and you do not need to account for different data types.
 
-const findShort = str => Math.min(...str.split(' ').map(str => str.length));
+// const findShort = str => Math.min(...str.split(' ').map(str => str.length));
 
-console.log('test1', findShort("bitcoin take over the world maybe who knows perhaps"));// 3
-console.log('test2',findShort("turns out random test cases are easier than writing out basic ones"))//3; 
+// console.log('test1', findShort("bitcoin take over the world maybe who knows perhaps"));// 3
+// console.log('test2',findShort("turns out random test cases are easier than writing out basic ones"))//3; 
+
+const fibonacci = num => {
+    var a = 1, b = 0, temp;
+  
+    while (num >= 0){
+      temp = a;
+      a = a + b;
+      b = temp;
+      num--;
+    }
+  
+    return b;
+  }
